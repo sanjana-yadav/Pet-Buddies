6d920a487a4a19851b44abe3aaac7317d4e7f776
sanjana yadav <sanjana02092001@gmail.com>
2024-11-05 13:28:51 +0530

    new file:   Server/config/db.js 	new file:   Server/config/generateToken.js 	new file:   Server/controllers/chatControllers.js 	new file:   Server/controllers/messageControllers.js 	new file:   Server/controllers/userControllers.js 	new file:   Server/data/data.js 	modified:   Server/index.js 	new file:   Server/middleware/authMiddleware.js 	new file:   Server/middleware/errorMiddleware.js 	new file:   Server/models/chatModel.js 	new file:   Server/models/messageModel.js 	new file:   Server/models/userModel.js 	new file:   Server/routes/chatRoutes.js 	new file:   Server/routes/messageRoutes.js 	new file:   Server/routes/userRoutes.js

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,16 @@ const morgan = require('morgan');
 const categoryRoutes = require('./routes/category');
 const petRoutes = require('./routes/pet');
 const adoptionRoutes = require('./routes/adoption');
+//Changes
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+dotenv.config();
+connectDB();
+//Changes end 
 
 const app = express();
 
@@ -19,6 +29,12 @@ app.use('/public', express.static(path.join(__dirname,'public')));
 app.use('/api/category',categoryRoutes);
 app.use('/api/pets',petRoutes);
 app.use('/api/adoption',adoptionRoutes);
+
+//new changes 
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
 
 const mongoUri = 'mongodb://localhost:27017/PetBuddiesDB';
 
@@ -35,4 +51,74 @@ mongoose.connection.on('error',(err)=>{
 
 app.listen(4001,() => {
     console.log("App is running on port 4001")
+});
+
+//new Changes
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 });
